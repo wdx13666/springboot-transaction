99d5e927b849705c8b46eb4991d49c2306dc13d4
wdx <872386515@qq.com>
2020-06-19 14:08:53 +0800

    Initial commit

---
+++ src/test/java/com/wdx/springboottransaction/SpringbootTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdx.springboottransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
